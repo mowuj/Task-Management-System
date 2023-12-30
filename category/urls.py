13ec0d287657c881b3b3f6bb7ea6330fec1149bb
mowuj <mowuj92@gmail.com>
2023-12-30 22:15:39 +0600

    create models and forms

---
+++ category/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # path('add_category', views.add_category, name='add_category'),
+]
--- task/models.py
+++ task/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from category.models import Category
 # Create your models here.
+class Task(models.Model):
+    task_title=models.CharField(max_length=50)
+    task_description=models.TextField()
+    is_completed=models.BooleanField(default=False)
+    assigned_date=models.DateTimeField(auto_now_add=True)
+    category=models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.task_title
