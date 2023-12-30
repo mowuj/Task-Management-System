f802b7b2893e763978d8292a22ab91ff0a0ef59b
mowuj <mowuj92@gmail.com>
2023-12-30 22:30:37 +0600

    views for task and category

--- category/views.py
+++ category/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import forms
+from .models import Category
 # Create your views here.
+
+def add_category(request):
+    if request.method=='POST':
+        category_form=forms.CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return redirect('home')
+    else:
+        category_form=forms.CategoryForm()
+    return render(request,'add_category.html',{'form':category_form})
+
+
--- task/views.py
+++ task/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import TaskForm
+from .models import Task
+# Create your views here.
+
 
 # Create your views here.
+
+def add_task(request):
+    if request.method=='POST':
+        task_form=TaskForm(request.POST)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('add_task')
+    else:
+        task_form=TaskForm()
+    return render(request,'add_task.html',{'form':task_form})
+
+def edit_task(request,id):
+    task=Task.objects.get(pk=id)
+    task_form=TaskForm(instance=task)
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST,instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('add_task')
+
+def delete_task(request, id):
+    task = Task.objects.get(pk=id)
+    task.delete()
+    return redirect('homepage')
